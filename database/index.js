d2958584fa3db7e7e2a970205150fdd410b03219
Adam Blomberg <adamablomberg@gmail.com>
2022-09-17 05:57:51 -0500

    completed route ofor reviews and able to access all 4 schema tables in pg admin

--- database/index.js
+++ database/index.js
@@ -2,36 +2,70 @@ const { Client } = require('pg');
 const Promise = require('bluebird');
 
 const connection = new Client({
-  host:'localhost',
-  user:'postgres',
-  password:'password',
-  port:'5432',
-  database:'obi-wan-reviews'
+  host: 'localhost',
+  user: 'postgres',
+  password: 'password',
+  port: '5432',
+  database: 'obi-wan-reviews'
 
 });
+
+//future store in the .env file
+
 const db = Promise.promisifyAll(connection, { multiArgs: true });
 
 db.connectAsync()
   .then(() => console.log('Connected to DB'))
   .catch((err) => console.log('Error connecting to the DB', err))
 
+const getReviewData = (product_Id, page = 1, count = 5, sort = 'helpfulness') => {
 
-const getReviews = (req,res) => {
+  if(sort === 'newest') {
+    sort = 'date'
+  }
 
-  // pool.query('SELECT * FROM review LIMIT 2')
-  // .then(result => {res.send(result)})
-  // .catch((err) => (console.log('error in getting reviews ', err)))
+// TODO:  refactor so that the there is an additional column in the table that is a combination of the helpfulness and the date or runs a query and then filters... not sure
+  if(sort === 'relevant') {
+    sort = 'helpfulness'
+  }
 
-  console.log('got reviews')
+//TODO:  activate the page number param to working order
+
   return db.queryAsync(
-    'SELECT * FROM review LIMIT 2')
-  .then((res) => {
-    console.log(res[0].rows[0].summary);
-    return res[0].rows[0].summary;
-  })
-  .catch((err) => (console.log('error in getting reviews',err)));
+    `SELECT review.id as review_id, review.product_id, review.rating, review.date, review.summary, review.body, review.recommend, review.reviewer_name, review.response, review.helpfulness,
+      (
+        SELECT array_to_json(coalesce(array_agg(photo), array[]::record[]))
+        from
+        (
+          SELECT photos.id, photos.url FROM public.photos WHERE photos.review_id = ${product_Id} ORDER BY id ASC LIMIT 5
+        ) photo
+      ) as photos
+
+    FROM public.review WHERE review.product_id=${product_Id} ORDER BY ${sort} DESC LIMIT ${count}`)
+    .then((res) => {
+      console.log(res[0].rows);
+      return {'product': product_Id, 'page':page, 'count':count, 'result':res[0].rows}
+    })
+    .then((res) => {
+      console.log(res);
+      return res
+    })
+    .catch((err) => (console.log('error in getting reviews', err)));
 }
 
-getReviews();
+const getReviewMeta = (req, res) => {
+  return db.queryAsync(
+    `SELECT * FROM public.characteristicreviews LIMIT 3`
+  )
+  .then((res) => {
+    console.log(res[0].rows);
+    return res
+  })
+  .catch((err) => (console.log('error in getting reviews', err)));
+}
 
-module.exports.getReviews = getReviews;
+getReviewMeta();
+
+//getReviewData(5,1,3, 'date')  //getReviewData query test
+module.exports.getReviewData = getReviewData;
+module.exports.db = db;
